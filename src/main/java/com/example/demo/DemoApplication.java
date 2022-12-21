11bbfa377863f8d95e30939541d5a8753b6b3034
Adriana Mondragón <106161619+amondragong25@users.noreply.github.com>
2022-12-21 13:54:26 -0600

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "Adriana, Hola mundo desde mi primer microservicio en Java 17 desde el IPN!";
     }
 
     public static void main(String[] args) {
